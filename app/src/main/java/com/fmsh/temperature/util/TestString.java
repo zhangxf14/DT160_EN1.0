ce26073409b0759f27d6d804e47cc9aeeaa004be
zhangxf14 <270274993@qq.com>
2020-09-10 13:44:16 +0800

    first commit

---
+++ app/src/main/java/com/fmsh/temperature/util/TestString.java
@@ -0,0 +1,44 @@
+package com.fmsh.temperature.util;
+
+import android.util.Base64;
+
+/**
+ * Created by wyj on 2018/10/15.
+ */
+public class TestString {
+
+    public static void main(String [] args) {
+//        byte[] transceive = {0x00, (byte) 0x80, (byte) 0xf7, (byte) 0x95,0x50};
+//        byte[] randomByte = new byte[4];
+//        randomByte[0] = transceive[2];
+//        randomByte[1] = transceive[4];
+//        randomByte[2] = transceive[1];
+//        randomByte[3] = transceive[3];
+//        String random = TransUtil.byteToHex(randomByte);
+//        random = TransUtil.hexStringToBinary(random);
+//        random = random.substring(random.length()-3)+random.substring(0,random.length()-3);
+//        String randomHex = TransUtil.binaryString2hexString(random);
+//        byte[] bytesRandom = TransUtil.hexStringToBytes(randomHex);
+//        String str = "000003";
+//
+//
+//        String substring = str.substring(str.length() - 4, str.length() - 2);
+//        String substring1 = str.substring(str.length() - 2, str.length());
+//        String newstr = substring1 + substring;
+//        String stringToBinary = TransUtil.hexStringToBinary(newstr);
+//        String tempData = stringToBinary.substring(stringToBinary.length() - 10);
+//        float resultTem = 0;
+//        String bStr = tempData.substring(1);
+//        String hexString = TransUtil.binaryString2hexString("0000000" + bStr);
+//        int parseInt = Integer.parseInt(hexString, 16);
+//        if(tempData.substring(0,1).equals("1")){
+//            parseInt = ~parseInt +1;
+//        }
+//        resultTem = (float) (parseInt /4.00);
+//        System.out.print(resultTem);
+
+
+
+
+    }
+}
